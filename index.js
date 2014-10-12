6375dc1e46230daa0b51747d14f8053f86fe0f4b
jonschlinkert <dev@sellside.com>
2014-10-12 02:14:01 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/*!
+ * omit-string <https://github.com/jonschlinkert/omit-string>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var typeOf = require('kind-of');
+
+module.exports = function (str, omit) {
+  omit = Array.isArray(omit) ? omit : [omit];
+
+  var len = omit.length;
+  for (var i = 0; i < len; i++) {
+    var token = omit[i];
+    var match = true;
+
+    while (match) {
+      if (typeOf(token) === 'regexp') {
+        match = token.test(str);
+      } else {
+        match = str.indexOf(token) !== -1;
+      }
+      str = str.replace(token, '');
+    }
+  }
+  return str;
+};
---
+++ test.js
@@ -0,0 +1,46 @@
+/*!
+ * string-omit <https://github.com/jonschlinkert/string-omit>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var omit = require('./');
+
+describe('omit', function () {
+  describe('string:', function () {
+    it('should remove the string.', function () {
+      omit('abcabcabcabcabcabc', 'a').should.equal('bcbcbcbcbcbc');
+    });
+  });
+
+  describe('array:', function () {
+    it('should remove each string in the array.', function () {
+      omit('abcabcabcabcabcabc', ['a', 'c']).should.equal('bbbbbb');
+      var fixture = 'this is a random sentence with words that can be omitted.';
+      var actual = omit(fixture, ['random', 'that', 'can', 'be', 'omitted'])
+      actual.should.equal('this is a  sentence with words    .');
+    });
+  });
+
+  describe('regex:', function () {
+    it('should use the regex pattern to replace values.', function () {
+      omit('abcabcabcabcabcabc', [/a/g, /c/g]).should.equal('bbbbbb');
+      var fixture = 'this is a random sentence with words that can be omitted.';
+      var re = /random|that|can|be|omitted/;
+      var actual = omit(fixture, re);
+      actual.should.equal('this is a  sentence with words    .');
+    });
+
+    it('should use the regex pattern to replace values.', function () {
+      var fixture = 'this is a random sentence with words that can be omitted.';
+      var re = /(?:random|that|can|be|omitted)|(\s{3,})/g;
+      var actual = omit(fixture, re);
+      actual.should.equal('this is a  sentence with words.');
+    });
+  });
+});
